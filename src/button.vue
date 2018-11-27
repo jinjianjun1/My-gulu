f3750aa4ce27872b74b9b669d0be64d15c1e7d61
jason <13141374484@163.com>
2018-11-27 23:26:40 +0800

    修改代码格式 且用parcel打包 将button改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="g-button">hi</button>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    }
+</script>
+
+<style  lang="sass">
+    .g-button
+        font-size: var(--font-size)
+        height: var(--button-height)
+        padding: 0 1em
+        border-radius:  var(--border-radius)
+        border: 1px solid var(--border-color)
+        background: var(--button-bg)
+        &:hover
+            border-color: var(--border-color-hover)
+
+        &:active
+            background-color: var(--button-active-bg)
+        &:focus
+            outline: none
+
+
+
+</style>
