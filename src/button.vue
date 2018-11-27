27e8c82beba85fc2a2344df757b939598239dd7d
jason <13141374484@163.com>
2018-11-27 23:52:09 +0800

    把sass改写scss

--- src/button.vue
+++ src/button.vue
@@ -8,22 +8,23 @@
     }
 </script>
 
-<style  lang="sass">
-    .g-button
-        font-size: var(--font-size)
-        height: var(--button-height)
-        padding: 0 1em
-        border-radius:  var(--border-radius)
-        border: 1px solid var(--border-color)
-        background: var(--button-bg)
-        &:hover
-            border-color: var(--border-color-hover)
-
-        &:active
-            background-color: var(--button-active-bg)
-        &:focus
-            outline: none
-
-
+<style  lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &: hover{
+        border-color: var(--border-color-hover)
+        }
+        &: active{
+        background-color: var(--button-active-bg)
+        }
+        &: focus{
+        outline: none
+        }
+    }
 
 </style>
