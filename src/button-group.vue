901547a0f2e900e109aa514e5c43ee6448bcf965
jason <13141374484@163.com>
2018-11-28 22:07:14 +0800

    检测子元素是否为button

--- src/button-group.vue
+++ src/button-group.vue
@@ -3,7 +3,18 @@
         <slot></slot>
     </div>
 </template>
-<script></script>
+<script>
+    export default {
+        mounted() {
+            for (let node of this.$el.children){
+              let name= node.nodeName.toLowerCase();
+                if(name!=='button'){
+                    console.warn(`g-button-group中只可以添加g-button,但是你写的是${name}`)
+                }
+            }
+        }
+    }
+</script>
 <style lang="scss">
     .g-button-group{
         display: inline-flex;vertical-align: middle;
