698857536e362a20c0ed7cffad540021694e4ff2
jason <13141374484@163.com>
2018-11-28 14:32:23 +0800

    添加svg icon

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,17 @@
 <template>
-    <button class="g-button">hi</button>
+
+    <button class="g-button">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <slot></slot>
+    </button>
 </template>
 
 <script>
     export default {
-        name: "Button"
+        name: "Button",
+        props:['icon','iconPosition']
     }
 </script>
 
@@ -16,15 +23,15 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &: hover{
-        border-color: var(--border-color-hover)
-        }
-        &: active{
-        background-color: var(--button-active-bg)
-        }
-        &: focus{
-        outline: none
-        }
+            &:hover{
+                border-color: var(--border-color-hover)
+            }
+            &:active{
+            background-color: var(--button-active-bg)
+            }
+            &:focus{
+            outline: none
+            }
     }
 
 </style>
