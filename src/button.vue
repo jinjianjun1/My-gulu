b3e9a071599a90ba3df512390412340fe1ae2f28
jason <13141374484@163.com>
2018-11-30 22:08:50 +0800

    input 样式

--- src/button.vue
+++ src/button.vue
@@ -12,7 +12,7 @@
 <script>
     import Icon from './icon'
     export default {
-        name: "Button",
+        name: "gulu-button",
         props:{
             loading:{type: Boolean,default: false},
             icon:{},
@@ -28,7 +28,7 @@
     }
 </script>
 
-<style  lang="scss">
+<style  lang="scss" scoped>
     @keyframes spin {
         0%{transform:rotate(0deg)}
         100%{transform: rotate(360deg)}
--- src/icon.vue
+++ src/icon.vue
@@ -6,9 +6,10 @@
 <script>
     import './svg'
     export default {
+        name:'gulu-icon',
         props:['name']
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     .g-icon {width: 1em;height: 1em;}
 </style>
