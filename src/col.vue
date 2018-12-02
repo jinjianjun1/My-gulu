a7655dcfb44385bcb5a4a269661adbb4e3bc2203
jason <13141374484@163.com>
2018-12-02 14:43:45 +0800

    重构col row

--- src/col.vue
+++ src/col.vue
@@ -1,21 +1,31 @@
 <template>
-    <div class="col" :class="[span &&`col-${span}`,offset &&`offset-${offset}`]"
-    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
-        <div style="border: 1px solid red">
+    <div class="col" :class="colClass" :style="colStyle">
             <slot></slot>
-        </div>
     </div>
 </template>
 
 <script>
     export default {
-    name:"GuluCol",
+        name:"GuluCol",
         props:{
         span:[String,Number],
         offset:[String,Number],
         },
         data(){
         return{gutter:0}
+        },
+        computed:{
+            colStyle(){
+                return {
+                    paddingLeft:this.gutter/2+'px',
+                    paddingRight:this.gutter/2+'px'
+                }},
+            colClass(){
+                let{span,offset}=this;
+                return [
+                    span &&`col-${span}`,
+                    offset &&`offset-${offset}`
+                ]}
         }
     }
 </script>
