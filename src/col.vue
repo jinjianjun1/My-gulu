e23e7c97ef75c1dffa8ba351a5cc65f5069ca480
jason <13141374484@163.com>
2018-12-03 11:36:24 +0800

    响应式2.0

--- src/col.vue
+++ src/col.vue
@@ -64,7 +64,7 @@
             margin-left: ($n/24)*100%;
         }
     }
-        @media (min-width: 577px) and(max-width: 768px) {
+        @media (min-width: 577px) {
             $class-prefix: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -77,7 +77,7 @@
                     margin-left: ($n/24)*100%;
                 }
             }}
-        @media (min-width: 769px) and(max-width: 992px) {
+        @media (min-width: 769px) {
             $class-prefix: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
@@ -90,7 +90,7 @@
                     margin-left: ($n/24)*100%;
                 }
             }}
-        @media (min-width: 993px) and(max-width: 1200px) {
+        @media (min-width: 993px) {
             $class-prefix: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class-prefix}#{$n} {
