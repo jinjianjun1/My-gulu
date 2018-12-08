b00e100e7db656a0249b726ddcc132e3fe996a14
jason <13141374484@163.com>
2018-12-08 22:29:09 +0800

    表编程 position逻辑

--- src/popover.vue
+++ src/popover.vue
@@ -28,22 +28,28 @@
             positionContent() {
                 const {contentWrapper,triggerWrapper}=this.$refs;
                 document.body.appendChild(contentWrapper);
-                let {left, width, height, top,bottom} = triggerWrapper.getBoundingClientRect();
-                if (this.position==='top'){
-                    contentWrapper.style.left = left + window.scrollX + 'px';
-                    contentWrapper.style.top = top + window.scrollY + 'px';
-                } else if (this.position==='bottom') {
-                    contentWrapper.style.left = left + window.scrollX + 'px';
-                    contentWrapper.style.top = top +height+ window.scrollY + 'px';
-                }else if(this.position==='left'){
-                    contentWrapper.style.left = left + window.scrollX + 'px';
-                    let {height:height2}=contentWrapper.getBoundingClientRect();
-                    contentWrapper.style.top=top+window.scrollY+(height-height2)/2+'px'
-                }else if (this.position === 'right'){
-                    contentWrapper.style.left = left + window.scrollX +width+ 'px';
-                    let {height:height2}=contentWrapper.getBoundingClientRect();
-                    contentWrapper.style.top=top+window.scrollY+(height-height2)/2+'px'
-                }
+                const {left, width, height, top,bottom} = triggerWrapper.getBoundingClientRect();
+                const {height:height2}=contentWrapper.getBoundingClientRect();
+                let positions={
+                    top:{
+                        top:top + window.scrollY,
+                        left:left + window.scrollX
+                    },
+                    bottom:{
+                        top:top +height+ window.scrollY,
+                        left:left + window.scrollX
+                    },
+                    left:{
+                        top:top+window.scrollY+(height-height2)/2,
+                        left:left + window.scrollX
+                    },
+                    right:{
+                        top:top+window.scrollY+(height-height2)/2,
+                        left:left + window.scrollX +width
+                    },
+                };
+                contentWrapper.style.left=positions[this.position].left+'px';
+                contentWrapper.style.top=positions[this.position].top+'px'
 
             },
             onClickEvent(e) {
@@ -126,10 +132,10 @@ $border-radius:4px;
             top: 50%;}
         &::before{
             border-right-color: black;
-            left: -16%;}
+            right: 100%;}
         &::after{
             border-right-color: white;
-            left: -15%;}
+            right : calc(100% - 1px)}
     }
 
 }
