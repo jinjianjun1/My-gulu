5bcee5538f8aa94462bc3ce61301f07fcbce9fe7
jason <13141374484@163.com>
2018-12-09 11:14:28 +0800

    添加插槽的close API

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="popover"  ref="popover">
         <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{[`position-${position}`]:true}">
-            <slot name="content"></slot>
+            <slot name="content"  :jason="1" :close="close"></slot>
         </div>
         <span  ref="triggerWrapper" style="display: inline-block;">
             <slot></slot>
@@ -59,7 +59,8 @@
                 validator(value){
                     return ['click','hover'].indexOf(value)>=0
                 }
-            }
+            },
+
         },
         methods:{
             positionContent() {
