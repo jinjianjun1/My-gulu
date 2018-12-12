f5202bc2e6996777a12fa73a05152707652f23cd
jason <13141374484@163.com>
2018-12-12 14:01:20 +0800

    使用vue cli3构建项目

--- README.md
+++ README.md
@@ -1,54 +1,34 @@
-## 制作vue ui框架 用于个人学习
-## 使用本框架请在css中开启 border-box
-[![Build Status](https://travis-ci.org/jinjianjun1/My-gulu.svg?branch=master)](https://travis-ci.org/jinjianjun1/My-gulu)
+# hello-world
 
-## 介绍
-学习vue 过程中制作的UI框架
+## Project setup
+```
+npm install
+```
 
-## 开始使用
-1.请在所有CSS前加入下面的代码
-  ```
-  *{box-size:border-box}
-  *::before{box-size:border-box}
-  *::after{box-size:border-box}
-  ```
-   设置默认颜色变量（后续改进为scss）
-  ````
-   :root{
-          --button-height:32px;
-          --font-size:14px;
-          --button-bg:#fff;
-          --button-active-bg:#eee;
-          --border-radius:4px;
-          --color:#333;
-          --border-color:#999;
-          --border-color-hover:#666;
-      }
-      #app{margin:20px}
-      body{
-          font-size: var(--font-size);
-      }
-  ````
-    IE15 以上支持
- 2.安装 clujun1-1
- ```
-    npm install clujun1-1
- ```
- 3.引入 clujun1-1
- ```
- import {Button,ButtonGroup,Icon} from 'clujun1-1'
- import 'clujun1-1/dist/index.css'
- 
- export default {
-   name: 'app',
-   components: {
-     HelloWorld,
-     'g-button':Button
-   }
- }
- ```
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
-## 文档
-## 变更记录
-## 联系方式
-## 贡献代码
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/demo.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <p>11111</p>
+        <g-cascader :source="source" height="200px">
+        </g-cascader>
+        <p>2222222</p>
+    </div>
+</template>
+
+<script>
+    import Button from './button'
+    import Cascader from './cascader'
+    export default {
+        name:'demo',
+        components:{
+            'g-button':Button,
+            'g-cascader':Cascader
+        },
+        data(){
+            return{
+                source:[{
+                    name:'山西',
+                    children:[
+                        {name:'临汾',
+                            children: [
+                                {name:'市辖区'},
+                                {name:'尧都区'},
+                                {name:'襄汾县'},
+                            ]
+                        },
+                        {name:'运城',
+                            children: [
+                                {name:'市辖区'},
+                                {name:'12312'},
+                                {name:'12431'},
+                            ]
+                        }
+                    ],
+                }, {name:'山东',
+                    children:[
+                        {name:'市名1',
+                            children: [
+                                {name:'区3'},
+                                {name:'区2'},
+                                {name:'区1'},
+                            ]
+                        },
+                        {name:'市名2',
+                            children: [
+                                {name:'市辖区'},
+                                {name:'12312'},
+                                {name:'12431'},
+                            ]
+                        }
+                    ],
+                }, {name:'杭州',
+                    children:[
+                        {name:'市名x',
+                            children: [
+                                {name:'区名q'},
+                                {name:'区名s'},
+                                {name:'区名v'},
+                            ]
+                        },
+                        {name:'市名ss',
+                            children: [
+                                {name:'市辖区'},
+                                {name:'区名sd'},
+                                {name:'区名szzz'},
+                            ]
+                        }
+                    ],
+                }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    *{margin: 0;padding:0;box-sizing: border-box}
+    :root{;--font-size:14px;}
+    body{
+        font-size: var(--font-size);
+    }
+</style>
