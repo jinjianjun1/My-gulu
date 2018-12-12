c364087a630dbbdb366c7e586e3459f8494c2802
jason <13141374484@163.com>
2018-12-12 18:17:29 +0800

    更新当前等级数据的同时删除后面等级的数据

--- src/cascader-item.vue
+++ src/cascader-item.vue
@@ -55,6 +55,7 @@
             onClickLabel(item){
                 let copy= JSON.parse(JSON.stringify(this.selected));
                 copy[this.level]=item;
+                copy.splice(this.level+1);
                 this.$emit('update:selected',copy);
             },
             onUpdateSelected(newSelected){
