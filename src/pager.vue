0bcab21e0d71ee358a121c35a8ea6401e005b793
jason <13141374484@163.com>
2018-12-24 16:17:13 +0800

    实现pager 基本结构和样式

--- src/pager.vue
+++ src/pager.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="gulu-pager">
-        <span v-for="page in pages">
+        <span  v-for="page in pages" class="gulu-pager-item" :class="{active:page===currentPage,separator:page==='...'}">
             {{page}}
         </span>
     </div>
@@ -10,9 +10,17 @@
     export default {
         name: "GuluPager",
         data(){
-            let pages=[1,this.currentPage,this.currentPage+1,this.totalPage,this.currentPage-1,this.currentPage-2,this.currentPage+2]
+            let pages=this.unique([1,this.currentPage,
+                this.currentPage+1,this.totalPage,
+                this.currentPage-1,this.currentPage-2, this.currentPage+2]
+                .filter(item=>item>0)
+                .sort((a,b)=>a-b)).reduce((prev,current,index,array)=>{
+                prev.push(current);
+                 (array[index+1]!==undefined && array[index+1]-array[index]>1)&&prev.push('...');
+                return prev
+            },[])
             return{
-                pages:this.unique(pages.sort((a,b)=>a-b))
+                pages
             }
         },
         props: {
@@ -34,7 +42,7 @@
                 let obj={};
                 array.map(number=>{
                     obj[number]=true
-                })
+                });
                 return Object.keys(obj).map((number)=>parseInt(number,10))
             }
         }
@@ -42,5 +50,22 @@
 </script>
 
 <style scoped lang="scss">
+    @import "var";
+    .gulu-pager{
+        &-item{
+            border: 1px solid #e1e1e1;border-radius: $border-radius;padding: 0 4px;display: inline-flex;
+            justify-content: center;align-items: center;min-width: 8px;height: 20px;margin: 0 4px;
+            font-size: 12px;cursor: pointer;
+            &.active,&:hover{
+                border-color: $blue;
+            }
+            &.active{
+                cursor: default;
+            }
+            &.separator{
+                border: none;
+            }
+        }
 
+    }
 </style>
