848506ee7f6e480d802e9f2786f71553a98dbcfa
jason <13141374484@163.com>
2019-02-08 16:45:26 +0800

    添加distance细节

--- src/sticky.vue
+++ src/sticky.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="j-sticky-wrapper" ref="wrapper" :style="{height}">
-        <div class="j-sticky" :class="classes" :style="{left,width}">
+        <div class="j-sticky" :class="classes" :style="{left,width,top}">
             <slot></slot>
         </div>
     </div>
@@ -9,48 +9,57 @@
 <script>
     export default {
         name: "sticky",
+        props:{
+            distance:{
+                type:Number,
+                default:0
+            }
+        },
         data() {
             return {
                 sticky: false,
                 left:undefined,
                 width:undefined,
-                height:undefined
+                height:undefined,
+                top:undefined
             }
-        },
-
-        created() {
-
-        },
+         },
         mounted(){
-             this.windowEvent=() => {
-                if (window.scrollY > top) {
-                    this.sticky=true;
-                    let {height,width,left} = this.$refs.wrapper.getBoundingClientRect();
-                    this.width=width+'px';
-                    this.left=left+'px';
-                    this.height=height+'px'
-
-                } else {
-                    this.sticky=false
-                }
-            };
-            let top =this.getTop();
-            window.addEventListener('scroll',this.windowEvent )
+            this.windowEvent=this._windowEvent.bind(this);
+         window.addEventListener('scroll',this.windowEvent )
         },
         beforeDestroy(){
-            window.removeEventListener('scroll',this.x)
-            console.log('hi');
-        },
-        updated(){
-            console.log(this.sticky);
+            window.removeEventListener('scroll',this.windowEvent)
         },
         methods: {
             getTop() {
                 let {top} = this.$refs.wrapper.getBoundingClientRect();
-                return top + scrollY
+                console.log(top);
+                return {top:top + window.scrollY}
+            },
+            _windowEvent() {
+                let {top} = this.getTop();
+                console.log(top,1);
+                if (window.scrollY > top - this.distance) {
+                    let {height, width, left} = this.$refs.wrapper.getBoundingClientRect();
+                    this.width = width + 'px';
+                    this.left = left + 'px';
+                    this.height = height + 'px';
+                    this.top =  this.distance + 'px';
+                    this.sticky = true;
+                } else {
+                    this.width = undefined;
+                    this.left = undefined;
+                    this.height = undefined;
+                    this.top = undefined;
+                    this.sticky = false;
+
+
+                }
             }
 
-        },
+
+    },
         computed: {
             classes() {
                 return {
@@ -64,9 +73,6 @@
 <style scoped lang="scss">
     .j-sticky.sticky{
         position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
     }
 
 </style>
